ad89ffcf8d388a293602093c55b5e1f670654054
Morris.Lin <Morris.Lin@nogle.com>
2022-09-29 15:51:49 +0800

    feat: LICENSE add Jacob Dent feat: refactor for browse_by_infinity and remove browse_by_close_open feat: refactor log wording

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 MIT License
 
-Copyright (c) 2022 MorrisLin
+Copyright (c) 2022 Jacob Dent & MorrisLin
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from service import browse_by_times, browse_by_infinity, browse_by_close_open
+from service import browse_by_times, browse_by_infinity
 
 if __name__ == "__main__":
     mode = input("choose mode 1: infinity mode 2: times mode 3: close-open node :")
+    times = 0
     if mode not in ("1", "2", "3"):
         print("wrong mode!")
         exit()
@@ -12,8 +13,8 @@ if __name__ == "__main__":
         print("empty url!")
         exit()
     if mode == "1":
-        browse_by_infinity(url)
+        browse_by_infinity(url, "change")
     elif mode == "2":
         browse_by_times(int(times), url)
     elif mode == "3":
-        browse_by_close_open(url)
+        browse_by_infinity(url, "close")
